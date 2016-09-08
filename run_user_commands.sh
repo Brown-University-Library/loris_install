aa7758f015a008c992179b157957c7164a5bfb35
bcail <bcail@localhost.localdomain>
2016-09-08 15:15:54 -0400

    add steps to configure loris

--- run_user_commands.sh
+++ run_user_commands.sh
@@ -2,8 +2,26 @@
 
 LORIS_DIR="/opt/local/loris"
 
+#copy in our custom configuration
+mkdir $LORIS_DIR/etc
+cp loris2.conf $LORIS_DIR/etc
+
 pushd $LORIS_DIR
-git clone https://github.com/loris-imageserver/loris.git src
+
+git clone https://github.com/loris-imageserver/loris.git loris_repo
+
+#setup other loris directories
+mkdir app_code
+mkdir etc
+mkdir bin
+mkdir lib
+mkdir cache
+mkdir log
+mkdir source_images
+mkdir www
+mkdir tmp
+
+cp loris_repo/loris/* app_code/
 
 #setup virtualenv
 virtualenv env
@@ -11,6 +29,8 @@ source ./env/bin/activate
 pip install --upgrade pip
 pip install Werkzeug
 pip install Pillow
+pip install configobj
+pip install requests
 pip list
 
 popd
